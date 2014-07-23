2a6b4927cc5dc00846a744c6123bb5d8944f6f94
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-07-23 17:41:12 +0200

    Log HTTP req/resp bodies in servlet filter

--- src/main/java/de/beosign/weatherstation/logging/FilterBeans.java
+++ src/main/java/de/beosign/weatherstation/logging/FilterBeans.java
@@ -6,9 +6,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterBeans {
+
     @Bean
     protected FilterRegistrationBean createTemperaturFilterRegistrationBean() {
         FilterRegistrationBean b = new FilterRegistrationBean(new TraceServletFilter());
+
+        b.addInitParameter("initparam", "test1");
+        b.addUrlPatterns("/temperaturereadings");
+        b.addUrlPatterns("/temperaturereadings/*");
+
+        return b;
+
+    }
+
+    @Bean
+    protected FilterRegistrationBean createLoggingFilterRegistrationBean() {
+        FilterRegistrationBean b = new FilterRegistrationBean(new LoggingFilter());
 
         b.addInitParameter("initparam", "test1");
         b.addUrlPatterns("/temperaturereadings");
