4026bfeeeab16cefa2685aed1e9f922804d3eb6c
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-07-26 20:32:26 +0200

    Set profile to PROD for web application

--- src/main/java/de/beosign/weatherstation/web/WeatherStationServletInitializer.java
+++ src/main/java/de/beosign/weatherstation/web/WeatherStationServletInitializer.java
@@ -1,13 +1,22 @@
 package de.beosign.weatherstation.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import de.beosign.weatherstation.Application;
+import de.beosign.weatherstation.spring.SpringProfiles;
 
 public class WeatherStationServletInitializer extends SpringBootServletInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeatherStationServletInitializer.class);
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        SpringApplicationBuilder b = application.sources(Application.class).profiles(SpringProfiles.PROFILE_PROD);
+
+        LOGGER.debug("Set active Spring profile to " + SpringProfiles.PROFILE_PROD);
+
+        return b;
     }
 }
