64606d4ccd7662e88c4096cb8560428b521ad45a
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-07-26 20:13:00 +0200

    Database Properties

---
+++ src/main/java/de/beosign/weatherstation/properties/DatabaseProperties.java
@@ -0,0 +1,43 @@
+package de.beosign.weatherstation.properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySources({ @PropertySource("classpath:/database.properties") })
+public class DatabaseProperties {
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+}
