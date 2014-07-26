45477fb850d26fd07e165385cd65e181473a9925
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-07-26 20:12:33 +0200

    Added Spring profiles

---
+++ src/main/java/de/beosign/weatherstation/spring/Production.java
@@ -0,0 +1,15 @@
+package de.beosign.weatherstation.spring;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Profile;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Profile(SpringProfiles.PROFILE_PROD)
+public @interface Production {
+
+}
---
+++ src/main/java/de/beosign/weatherstation/spring/SpringProfiles.java
@@ -0,0 +1,10 @@
+package de.beosign.weatherstation.spring;
+
+public class SpringProfiles {
+    public static final String PROFILE_DEV = "dev";
+    public static final String PROFILE_PROD = "prod";
+
+    private SpringProfiles() {
+    }
+
+}
