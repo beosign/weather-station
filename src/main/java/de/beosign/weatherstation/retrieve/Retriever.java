b1c755419d16dd0a9348f6b09fa70fa2e449d677
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-10-27 22:32:01 +0100

    JavaDoc

--- src/main/java/de/beosign/weatherstation/retrieve/Retriever.java
+++ src/main/java/de/beosign/weatherstation/retrieve/Retriever.java
@@ -1,5 +1,11 @@
 package de.beosign.weatherstation.retrieve;
 
+/**
+ * Classes that retrieve a value from a sensor should extend/implement that interface.
+ * 
+ * @author Florian Dahlmanns
+ * @param <T> The type of the value that is retrieved, e.g. {@link Double} for a temperature.
+ */
 public interface Retriever<T> {
     /**
      * Retrieves a value from a sensor.
