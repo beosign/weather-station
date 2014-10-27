cfdee707ec65e3c7a7b86034009fda7e02e075bf
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-10-27 22:30:57 +0100

    Changed from public to protected no-arg constructor

--- src/main/java/de/beosign/weatherstation/jpa/JPAEntity.java
+++ src/main/java/de/beosign/weatherstation/jpa/JPAEntity.java
@@ -6,29 +6,59 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
+/**
+ * Common superclass for entities. Defines the id and version fields.
+ * 
+ * @author Florian Dahlmanns
+ */
 @MappedSuperclass
 public class JPAEntity {
-
     private Long id;
-
     private Long version;
 
+    /**
+     * No-arg constructor is necessary for JPA.
+     */
+    protected JPAEntity() {
+
+    }
+
+    /**
+     * Returns the id value.
+     * 
+     * @return id
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
         return id;
     }
 
+    /**
+     * Returns the version.
+     * 
+     * @return version
+     */
     @Version
     public Long getVersion() {
         return version;
     }
 
+    /**
+     * Necessary for JPA.
+     * 
+     * @param id id
+     */
     @SuppressWarnings("unused")
     private void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Necessary for JPA.
+     * 
+     * @param version version
+     */
     @SuppressWarnings("unused")
     private void setVersion(Long version) {
         this.version = version;
