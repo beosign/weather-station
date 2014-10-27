84f4b5dd9f09c8092eb3600d7429e37c62372e59
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-10-27 22:31:46 +0100

    Moved method to super interface

--- src/main/java/de/beosign/weatherstation/reading/ReadingRepository.java
+++ src/main/java/de/beosign/weatherstation/reading/ReadingRepository.java
@@ -1,14 +1,29 @@
 package de.beosign.weatherstation.reading;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
+/**
+ * Base interface for {@link Reading} repositories. Annotated with {@link NoRepositoryBean} as this interface does not define a concrete repository.
+ * 
+ * @author Florian Dahlmanns
+ * @param <S> Concrete {@link Reading} type, like {@link TemperatureReading}.
+ */
 @NoRepositoryBean
 public interface ReadingRepository<S extends Reading<?>> extends CrudRepository<S, Long> {
-    // List<S> getBefore(Date date);
-    //
-    // List<Reading<S>> getAfter(Date date);
-    //
-    // List<Reading<S>> getBetween(Date fromDate, Date toDate);
+    /**
+     * Returns all readings within a given date interval.
+     * 
+     * @param fromDate start date
+     * @param toDate end date
+     * @return all readings within the interval.
+     */
+    List<S> findByReadDateBetween(@Param("fromDate") @DateTimeFormat(iso = ISO.DATE) Date fromDate, @Param("toDate") @DateTimeFormat(iso = ISO.DATE) Date toDate);
 
 }
--- src/main/java/de/beosign/weatherstation/reading/TemperatureReadingRepository.java
+++ src/main/java/de/beosign/weatherstation/reading/TemperatureReadingRepository.java
@@ -1,12 +1,6 @@
 package de.beosign.weatherstation.reading;
 
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 /**
  * Rest and JPA repository.
@@ -15,6 +9,5 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
  */
 @RepositoryRestResource(path = "temperaturereadings")
 public interface TemperatureReadingRepository extends ReadingRepository<TemperatureReading> {
-    List<TemperatureReading> findByReadDateBetween(@Param("fromDate") @DateTimeFormat(iso = ISO.DATE) Date fromDate, @Param("toDate") @DateTimeFormat(
-            iso = ISO.DATE) Date toDate);
+
 }
