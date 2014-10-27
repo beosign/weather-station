5d8c3659f2403a8d6f5af28e8fe2f3e8fe625a7a
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-10-27 22:29:54 +0100

    Changed to unchecked exception

--- src/main/java/de/beosign/weatherstation/retrieve/RetrieveException.java
+++ src/main/java/de/beosign/weatherstation/retrieve/RetrieveException.java
@@ -1,24 +1,56 @@
 package de.beosign.weatherstation.retrieve;
 
-public class RetrieveException extends Exception {
+/**
+ * Thrown to indicate that a retrieval operation from a sensor could not be executed. This exception is usually thrown from a {@link TemperatureRetriever}.
+ * 
+ * @author Florian Dahlmanns
+ */
+public class RetrieveException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Instantiates a new retrieve exception.
+     */
     public RetrieveException() {
         super();
     }
 
+    /**
+     * Instantiates a new retrieve exception.
+     *
+     * @param message the message
+     * @param cause the cause
+     * @param enableSuppression the enable suppression
+     * @param writableStackTrace the writable stack trace
+     */
     public RetrieveException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 
+    /**
+     * Instantiates a new retrieve exception.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
     public RetrieveException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Instantiates a new retrieve exception.
+     *
+     * @param message the message
+     */
     public RetrieveException(String message) {
         super(message);
     }
 
+    /**
+     * Instantiates a new retrieve exception.
+     *
+     * @param cause the cause
+     */
     public RetrieveException(Throwable cause) {
         super(cause);
     }
